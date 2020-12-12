61b23769788d6b495e0e3d46b8452c29d1fbabb6
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-12 09:14:36 +0000

    Extract <Board /> and <Square /> components from index file

--- src/index.js
+++ src/index.js
@@ -1,53 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Board from '../components/Board'
 import './index.css';
-
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="square"
-//         onClick={() => this.props.onClick()}
-//       >
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  } 
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Game extends React.Component {
 
@@ -154,7 +109,6 @@ function Square(props) {
     </button>
   );
 }
-// ========================================
 
 ReactDOM.render(
   <Game />,
